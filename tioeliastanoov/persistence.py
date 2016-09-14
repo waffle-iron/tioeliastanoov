3e5e66f1ce671d26231a2eea48818bdb7ba4657d
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2016-09-13 23:27:05 -0300

    Create TioEliasStatus model

--- tioeliastanoov/persistence.py
+++ tioeliastanoov/persistence.py
@@ -1,9 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Datetime, Integer
 
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
 Session = sessionmaker(bind=engine)
+
+
+class TioEliasStatus(Base):
+    datetime = Column(Datetime, primary_key=True)
+    status = Column(Integer, nullable=False)
 
