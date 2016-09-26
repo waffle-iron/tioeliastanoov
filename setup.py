435d99579b7e4e1d752b1311399a97fb4fcf7d62
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2016-09-26 02:29:28 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+__version__ = '0.0.1'
+
+
+setup(
+    name='tioeliastanoov',
+    version=__version__,
+    description='Tio Elias ta no OV?',
+    author='Elias Tandel',
+    author_email='elias.tandel@gmail.com',
+    url='https://github.com/etandel/tioeliastanoov',
+    download_url='https://codeload.github.com/etandel/tioeliastanoov/tar.gz/' + __version__,
+    packages=['tioeliastanoov'],
+)
+
