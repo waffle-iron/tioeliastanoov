041486d61d5cbf64f4b3513b7cecceb74e8089f6
Elias Tandel Barrionovo <elias.tandel@gmail.com>
2016-09-25 23:22:43 -0300

    Use Alchemytools

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from unittest import TestCase, main
 
-#from alchemytools.context import managed
+from alchemytools.context import managed
 from freezegun import freeze_time
 
 from tioeliastanoov.exceptions import InvalidStatusError
@@ -18,22 +18,20 @@ class TestChangeStatus(TestCase):
         Base.metadata.drop_all()
 
     def test_invalid_stats(self):
-        s = Session()
-        self.assertRaises(InvalidStatusError, change_status, s, 0)
-        self.assertRaises(InvalidStatusError, change_status, s, 5)
-        self.assertRaises(InvalidStatusError, change_status, s, -1)
+        with managed(Session) as s:
+            self.assertRaises(InvalidStatusError, change_status, s, 0)
+            self.assertRaises(InvalidStatusError, change_status, s, 5)
+            self.assertRaises(InvalidStatusError, change_status, s, -1)
 
     @freeze_time(datetime.utcnow())
     def test_change_status(self):
-        s = Session()
-        change_status(s, TioEliasStatus.unavailable.value)
-        s.commit()
+        with managed(Session) as s:
+            change_status(s, TioEliasStatus.unavailable.value)
 
-        s = Session()
-        status_change = s.query(TioEliasStatusChange).one()
-        self.assertEqual(status_change.datetime, datetime.utcnow())
-        self.assertEqual(status_change.status,
-                         TioEliasStatus.unavailable.value)
+        with managed(Session) as s:
+            status_change = s.query(TioEliasStatusChange).one()
+            self.assertEqual(status_change.datetime, datetime.utcnow())
+            self.assertEqual(status_change.status,
+                             TioEliasStatus.unavailable.value)
 
 
-main()
